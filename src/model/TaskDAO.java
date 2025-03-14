ebb4a5d838b1d39a0acd388930d4b917eb3efbd0
alaazrk <zalaazerrouki@gmail.com>
2025-03-14 19:05:28 +0100

    Les classes DAO

--- src/model/TaskDAO.java
+++ src/model/TaskDAO.java
@@ -1,5 +1,66 @@
 package model;
 
-public class TaskDAO {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class TaskDAO {
+	public List<Task> getTasksByUserId(int userId) throws SQLException, IOException {
+        List<Task> tasks = new ArrayList<>();
+        String sql = "SELECT * FROM tasks WHERE user_id = ?";
+        try (Connection conn = DataConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, userId);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                tasks.add(new Task(rs.getInt("id"), rs.getString("title"), rs.getString("description"),
+                        rs.getDate("due_date").toLocalDate(), rs.getString("status"), rs.getInt("user_id")));
+            }
+        }
+        return tasks;
+    }
+
+    public void addTask(Task task) throws SQLException, IOException {
+        String sql = "INSERT INTO tasks (title, description, due_date, status, user_id) VALUES (?, ?, ?, ?, ?)";
+        try (Connection conn = DataConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, task.getTitle());
+            pstmt.setString(2, task.getDescription());
+            pstmt.setDate(3, java.sql.Date.valueOf(task.getDueDate()));
+            pstmt.setString(4, task.getStatus());
+            pstmt.setInt(5, task.getUserId());
+            pstmt.executeUpdate();
+        }
+    }
+
+	public void updateTask(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Task> getAllTasks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteTask (int taskId)throws SQLException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Task> getTasksByStatus(int userId, String status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Task> getTasksSortedByDueDate(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+   
 }
--- src/model/UserDAO.java
+++ src/model/UserDAO.java
@@ -1,5 +1,31 @@
 package model;
 
-public class UserDAO {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class UserDAO {
+    public User getUserByUsername(String username) throws SQLException, IOException {
+           String sql = "SELECT * FROM users WHERE username = ?";
+           try (Connection conn = DataConnection.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+               pstmt.setString(1, username);
+               ResultSet rs = pstmt.executeQuery();
+               if (rs.next()) {
+                   return new User(rs.getInt("id"), rs.getString("username"), rs.getString("password"));
+               }
+           }
+           return null;
+       }
+    public void addUser(User user) throws SQLException, IOException {
+        String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        try (Connection conn = DataConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUsername());
+            pstmt.setString(2, user.getPassword());
+            pstmt.executeUpdate();
+        }
+    }
 }
