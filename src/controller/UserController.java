9967fae4692cf7b03cfba36b820e746b87b18cf2
wafaxafra <melissamansouri713@gmail.com>
2025-03-19 23:34:18 +0100

    Update UserController.java

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,22 +1,48 @@
 package controller;
 
+import java.io.IOException;
+
+import java.sql.SQLException;
 import model.User;
 import model.UserDAO;
-import java.io.IOException;
-import java.sql.SQLException;
+import view.UserView;
 
 public class UserController {
-    private UserDAO userDAO;
 
-    public UserController() {
-        this.userDAO = new UserDAO();
-    }
+     private UserDAO userDAO;
+        private UserView userView;
 
-    public void addUser(User user) throws SQLException, IOException {
-        userDAO.addUser(user);
-    }
+        public UserController() {
+            this.userDAO = new UserDAO();
+            this.userView = new UserView();
+        }
 
-    public User getUserByUsername(String username) throws SQLException, IOException {
-        return userDAO.getUserByUsername(username);
-    }
+        public User login(String username, String password) {
+            try {
+
+                User user = userDAO.getUserByUsername(username);
+                if (user != null && user.getPassword().equals(password)) {
+
+                    return user; // Connexion réussie 
+                }
+            } catch (SQLException | IOException e) {
+                System.err.println("Erreur lors de la connexion : " + e.getMessage());
+            }
+            return null; // Échec de la connexion
+        }
+
+        public boolean register(String username, String password) {
+            User user = new User(username, password);
+            try {
+                userDAO.addUser(user);
+                System.out.println("Utilisateur enregistré avec succès.");
+                return true;
+
+
+            } catch (SQLException | IOException e) {
+                System.err.println("Erreur lors de l'enregistrement de l'utilisateur : " + e.getMessage());
+                return false;
+            }
+        }
+
 }
